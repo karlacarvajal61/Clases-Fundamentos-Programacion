f723febba9184bb54607e1c00933a28d5e8ecf3e
Karla Margarita Carvajal Alpízar <karla.carvajal@lab-a3-18.cetav.local>
2026-03-11 13:50:39 -0600

    Add

---
+++ Tarea1/main.js
@@ -0,0 +1,21 @@
+const number = 39;
+let min1 = 10;
+let max1 = 20;
+let min2 = 40;
+let max2 = 60;
+
+let result = (number >= min1 && number <= max1, number >=min2 && number <= max2)
+let respuesta = 'El cálculo de rangos para el número'
+let respuesta2 = 'es:'
+
+console.log (respuesta, number, respuesta2, result)
+
+
+let dogAge = 7;
+let humanAge = ((2*10)+((dogAge-2)*4))
+
+let respuesta3 = 'El perrito de'
+let respuesta4 = 'años tendría'
+let respuesta5 = 'si fuera humano'
+
+console.log (respuesta3,dogAge,respuesta4,humanAge,respuesta5)
